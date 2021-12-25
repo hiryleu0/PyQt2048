e63ec3a98009428fc26fb602490b8ace0a8565fa
Mikołaj Ryll <01142207@pw.edu.pl>
2021-12-25 01:20:50 +0100

    Dummy key pressing handlers

---
+++ constants.py
@@ -0,0 +1,19 @@
+BUTTON_SIZE = 60
+BUTTON_MARGIN = 5
+BUTTONS_IN_ROW = 6
+
+COLOR = {
+    0: "#FFFFFF",
+    1: "#FCFFFC",
+    2: "#F8FFF8",
+    4: "#EFFFEF",
+    8: "#E0FFE0",
+    16: "#D0FFD0",
+    32: "#C0FFC0",
+    64: "#A8FFA8",
+    128: "#90EF90",
+    256: "#70DF70",
+    512: "#50CF50",
+    1024: "#20BF20",
+    2048: "#00AF00",
+}
--- main.py
+++ main.py
@@ -1,16 +1,86 @@
-# This is a sample Python script.
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from random import shuffle
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from constants import *
+
+LabelGrid = list[list[QLabel]]
+labels: LabelGrid = []
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def generate_new_two():
+    xs = [x for x in range(BUTTONS_IN_ROW)]
+    ys = [y for y in range(BUTTONS_IN_ROW)]
+    shuffle(xs)
+    shuffle(ys)
+
+    for x in xs:
+        for y in ys:
+            label = labels[x][y]
+            if label.text() == '0':
+                update_label(label, 2)
+                return
 
 
-# Press the green button in the gutter to run the script.
+def update_label(label: QLabel, value: int):
+    label.setText(str(value))
+    label.setStyleSheet('background-color: ' + COLOR[value] + ';'
+                                                              'border: 2px solid black;'
+                                                              'border-radius: 5px;')
+    label.repaint()
+
+
+def key_press_event(event: QKeyEvent):
+    if event.key() == Qt.Key.Key_W:
+        label = labels[0][0]
+        value = int(label.text())
+        new_value = 1 if value == 0 else value * 2
+        update_label(label, new_value)
+
+    elif event.key() == Qt.Key.Key_S:
+        print('S')
+    elif event.key() == Qt.Key.Key_A:
+        print('A')
+    elif event.key() == Qt.Key.Key_D:
+        print('D')
+
+    generate_new_two()
+
+
+def window():
+    app = QApplication(sys.argv)
+    widget = QWidget()
+    widget_size = BUTTONS_IN_ROW * BUTTON_SIZE + (BUTTONS_IN_ROW + 1) * BUTTON_MARGIN
+    widget.setFixedSize(widget_size, widget_size)
+    widget.keyPressEvent = key_press_event
+
+    shift = BUTTON_MARGIN + BUTTON_SIZE
+    for i in range(BUTTONS_IN_ROW):
+        labels_row = []
+        for j in range(BUTTONS_IN_ROW):
+            label = QLabel(widget)
+            label.setFixedSize(BUTTON_SIZE, BUTTON_SIZE)
+
+            x_shift = BUTTON_MARGIN + i * shift
+            y_shift = BUTTON_MARGIN + j * shift
+            label.move(x_shift, y_shift)
+            label.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+            label.setAlignment(Qt.AlignCenter)
+            label.setText('0')
+            label.setStyleSheet('background-color: ' + COLOR[0] + ';'
+                                                                  'border: 2px solid black;'
+                                                                  'border-radius: 5px;')
+            labels_row.append(label)
+        labels.append(labels_row)
+
+    generate_new_two()
+    widget.move(50, 50)
+    widget.setWindowTitle("PyGt5 2048")
+    widget.show()
+    sys.exit(app.exec_())
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    window()
