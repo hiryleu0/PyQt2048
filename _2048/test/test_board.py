261e5a2fe8fce5ba3630407e057e1c5fb4f8a353
Mikołaj Ryll <01142207@pw.edu.pl>
2021-12-26 22:03:49 +0100

    Board module tested

--- _2048/test/test_board.py
+++ _2048/test/test_board.py
@@ -104,3 +104,73 @@ class TestBoard(unittest.TestCase):
         for i in range(4):
             board.make_move(i)
             self.assertEqual(board.board, expected_boards[i])
+
+    def test_is_won(self):
+        board = Board(3)
+        board.board = [
+            [0, 2, 4],
+            [2, 4, 0],
+            [16, 32, 64]
+        ]
+        self.assertTrue(board.is_won(32))
+        self.assertTrue(board.is_won(64))
+        self.assertFalse(board.is_won(128))
+
+    def test_is_lost(self):
+        board = Board(3)
+        board.board = [
+            [4, 2, 4],
+            [2, 4, 2],
+            [16, 32, 64]
+        ]
+        self.assertTrue(board.is_lost())
+
+    def test_is_not_lost(self):
+        board = Board(3)
+        board.board = [
+            [4, 2, 4],
+            [2, 4, 4],
+            [16, 32, 64]
+        ]
+        self.assertFalse(board.is_lost())
+
+    def test_generate_random_two_on_empty_board(self):
+        board = Board(3)
+        board.generate_random_two()
+        zeros = 0
+        twos = 0
+        for i in range(board.size):
+            for j in range(board.size):
+                if board.board[i][j] == 0:
+                    zeros = zeros + 1
+                elif board.board[i][j] == 2:
+                    twos = twos + 1
+        self.assertEqual(zeros, board.size * board.size - 1)
+        self.assertEqual(twos, 1)
+
+    def test_generate_random_two_with_one_empty_slot(self):
+        board = Board(3)
+        specific_board = [
+            [2, 0, 4],
+            [4, 8, 16],
+            [16, 32, 2],
+        ]
+        expected_board = [
+            [2, 2, 4],
+            [4, 8, 16],
+            [16, 32, 2],
+        ]
+        board.board = specific_board
+        board.generate_random_two()
+        self.assertEqual(board.board, expected_board)
+
+    def test_generate_random_two_on_full_board(self):
+        board = Board(3)
+        full_board = [
+            [4, 8, 16],
+            [16, 32, 64],
+            [32, 64, 128],
+        ]
+        board.board = full_board
+        board.generate_random_two()
+        self.assertEqual(board.board, full_board)
